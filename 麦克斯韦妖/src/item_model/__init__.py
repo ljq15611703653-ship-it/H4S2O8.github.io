6d3349d330e4a97689fd29aa2a4b1daf53325a68
H4S2O8 <69383190+H4S2O8@users.noreply.github.com>
2026-08-20 22:48:32 +0800

    Export item model API

---
+++ 麦克斯韦妖/src/item_model/__init__.py
@@ -0,0 +1,11 @@
+from .model import Axis, Item, Process, transfer
+from .reactions import resolve_contact, resolve_intrinsic_reactions
+
+__all__ = [
+    "Axis",
+    "Item",
+    "Process",
+    "transfer",
+    "resolve_contact",
+    "resolve_intrinsic_reactions",
+]
